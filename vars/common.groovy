64ea1dce1038f02ec65a05ed908570de63917935
gteijema <g.teijema@afterpay.nl>
2019-05-13 15:16:20 +0200

    add common vars class to library

--- vars/common.groovy
+++ vars/common.groovy
@@ -1,16 +1,21 @@
-class common {
-    def config = [
-            'master': new props('prod', false),
-            'feature-jenkins-pipeline-as-code': new props('dev', true)
+class Common {
+    final def config = [
+            'master': new Property(Environment.PROD, false),
+            'feature-jenkins-pipeline-as-code': new Property(Environment.DEV, true)
         ]
 }
 
-class props {
-    def env
+class Property {
+    Environment env
     boolean snapshot
 
-    props(def env, boolean snapshot) {
+    Property(Environment env, boolean snapshot) {
         this.env = env
         this.snapshot = snapshot
     }
+}
+
+enum Environment {
+    PROD,
+    DEV
 }
