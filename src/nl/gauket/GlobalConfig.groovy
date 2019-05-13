361c92eb289532fff76eaa1eade66d04e01ef65a
gteijema <g.teijema@afterpay.nl>
2019-05-13 16:28:58 +0200

    add Global vars class to library

--- src/nl/gauket/GlobalConfig.groovy
+++ src/nl/gauket/GlobalConfig.groovy
@@ -1,11 +1,15 @@
 package nl.gauket
 
 class GlobalConfig {
+    // version property for demo purpose
     static String library_version = "0.0.1"
 
+    // mapping of branch to environment
     static def config = [
-            'master'                          : 'prod',
-            'feature-jenkins-pipeline-as-code': 'dev'
+            'master'     : 'prod',
+            'integration': 'test',
+            'bugfix-*'   : 'dev',
+            'feature-*'  : 'dev'
     ]
 
 }
